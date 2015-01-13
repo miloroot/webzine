3023e71eb895d380bb4238d57222c3f7359ec9bf
miloroot <milonice@hotmail.com>
2015-01-13 15:59:03 +0100

    Added click-handler to Log in-button for fun. And see if it's reachable.

--- js/main.js
+++ js/main.js
@@ -3,4 +3,15 @@ $(function() {
 	// DOM-Ready and working? : YES.
 	console.log('Hello world, this is a webzine.');
 
+	//Just a thing for fun! 
+	$('.login').click(function() {
+		var pw = prompt('Enter correct password:');
+		if(pw == "google") {
+			alert('Correct password!'); 
+		}
+		else {
+			alert('Bad password!');
+		}
+	});
+
 });
