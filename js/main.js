b9ae0bfde118fdc9042b8b6002cdd15c41e8c711
miloroot <milonice@hotmail.com>
2015-01-15 09:40:54 +0100

    Added a dummy-login function via a prompt(); If password is correct, jQuery .load will load admin.html.

--- js/main.js
+++ js/main.js
@@ -7,7 +7,9 @@ $(function() {
 	$('.login').click(function() {
 		var pw = prompt('Enter correct password:');
 		if(pw == "google") {
-			alert('Correct password!'); 
+			$('body').load('admin.html', function() {
+				alert('You logged in successfully!');
+			});
 		}
 		else {
 			alert('Bad password!');
