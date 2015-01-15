c867c82aa95319490ace4aa1872a51fea62a9038
miloroot <milonice@hotmail.com>
2015-01-15 14:22:44 +0100

    Working search function. Only needs a few tweaks.

--- php/data.php
+++ php/data.php
@@ -4,19 +4,20 @@
 	$host = 'mysql:host=127.0.0.1;dbname=nerdzine;';
 	$dbname = 'root';
 	$dbpass = 'mysql';
-	$options = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'");
+	$options = array(
+		//PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'",
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+	);
 
 	try {
 		$pdo = new PDO($host, $dbname, $dbpass, $options);
 
 		// SQL-questions to search after a page
-		$sql_getPages = 'SELECT * FROM pages WHERE title = ":search_param"';
+		$sql_getPages = 'SELECT * FROM pages WHERE title LIKE :search_param';
 		$dbh = $pdo->prepare($sql_getPages);
-		$dbh->execute([':search_param' => $search_param]);
-		// $dbh->execute([':search_param' => $_REQUEST['search_param']]);
+		$dbh->execute([':search_param' => '%'.$search_param."%"]);
 		$data = $dbh->fetchAll();
 		echo json_encode($data);
-		// print_r($data);
-	} catch(PDOException $e) {
+	} catch(PDOexception $e) {
 		echo $e->getMessage();
 	}
