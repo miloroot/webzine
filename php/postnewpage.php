87fed2c727ec4fabd3f3598ebd3bec05bcd43534
miloroot <milonice@hotmail.com>
2015-02-05 13:37:41 +0100

    Now solved. The form adds data to both pages and url_alias -tables. Now need to make so all sites are .load()/ajax'ed.

--- php/postnewpage.php
+++ php/postnewpage.php
@@ -15,15 +15,27 @@
 	];
 
 	$sql_postNewPage = 
-	'INSERT INTO pages (title, body, user_id) VALUES (:title, :body, :user_id);
-	 INSERT INTO  url_alias(pathen) VALUES (:pathen);';
+	'INSERT INTO pages (title, body, user_id) VALUES (:title, :body, :user_id);';
 
-	 $params = [
+	 $params_pages = [
 	 	':title' => $body['title'],
 	 	':body' => $body['body'],
-	 	':user_id' => $body['user_id'],
-	 	':pathen' => $body['pathen']
+	 	':user_id' => $body['user_id']
 	 ];
 
 	 $dbh_pnp = $pdo->prepare($sql_postNewPage);
-	 $dbh_pnp->execute($params);
+	 $dbh_pnp->execute($params_pages);
+	 $pid = $pdo->lastInsertId();
+
+
+	 // this chunk will send to url_alias table
+	 $sql_newPageUrl =
+	 'INSERT INTO url_alias (pathen, pid) VALUES (:pathen, :pid);';
+
+	 $params_url_alias = [
+	 	':pathen' => $body['pathen'],
+	 	':pid' => $pid
+	 ];
+
+	 $dbh_pnp = $pdo->prepare($sql_newPageUrl);
+	 $dbh_pnp->execute($params_url_alias);
